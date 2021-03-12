3364662810d534b828f5817904d988fed4cb38f2
Splint <79016046+Splintaz@users.noreply.github.com>
2021-03-12 18:32:37 +0100

    __init__.py

--- post/__init__.py
+++ post/__init__.py
@@ -1,8 +1,10 @@
+ 
+# O-Pac (Office Program and Control) allows you to send remote commands and receive encrypted data with a Word/Excel/PowerPoint Macro.
+# Created by https://github.com/Splintaz/
 from flask import Flask
-
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "wordpac" # Change the secret key for security reasons
+    app.config["SECRET_KEY"] = "officepac" # Change the secret key for security reasons
 
     from .views import views
     app.register_blueprint(views, url_prefix="/")
