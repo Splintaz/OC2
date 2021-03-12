a087566395849e3d5f7f96a55b2c902ef096269f
Splint <79016046+Splintaz@users.noreply.github.com>
2021-03-12 18:34:20 +0100

    request.py
    changed localhost to 127.0.0.1 :)

--- extra/request.py
+++ extra/request.py
@@ -1,5 +1,7 @@
+# O-Pac (Office Program and Control) allows you to send remote commands and receive encrypted data with a Word/Excel/PowerPoint Macro.
+# Created by https://github.com/Splintaz/
 import requests
 dictToSend = "Hello" # String to send
-res = requests.post("http://localhost:5000/python", json=dictToSend)
+res = requests.post("http://127.0.0.1:5000/python", json=dictToSend)
 dictFromServer = res.json() 
 print(f"Response from server: {dictFromServer}") # dictToReturn sends us the server string
