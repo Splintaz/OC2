22212706302b1c1f11e6f0b5928f8f5e153d5342
Splint <79016046+Splintaz@users.noreply.github.com>
2021-12-13 15:08:09 +0100

    request.py

--- extra/request.py
+++ extra/request.py
@@ -1,4 +1,4 @@
-# O-Pac (Office Program and Control) allows you to send remote commands and receive encrypted data with a Word/Excel/PowerPoint Macro.
+# OC2 (Office Command and Control) allows you to send remote commands and receive encrypted data with a Word/Excel/PowerPoint Macro.
 # Created by https://github.com/Splintaz/
 import requests
 dictToSend = "Hello" # String to send
