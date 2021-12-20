3f329831996c7df2f2582d1b31d6a9c54faf13ce
Splint <79016046+Splintaz@users.noreply.github.com>
2021-12-20 21:53:37 +0100

    main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from colorama import Fore, Back, Style
 from post import create_app
 app = create_app()
 def opac():
-    print(Fore.GREEN + """
+    print(Fore.CYAN + """
     Office Command and Control
 Created by https://github.com/Splintaz
 """)
     print(Style.RESET_ALL)
 if __name__ == "__main__":
     opac()
-    app.run(host="0.0.0.0", port="1234")
+    app.run(host="0.0.0.0", port="4444")
